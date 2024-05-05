96c27c012a601330ed14581ea7f91cd281e1b357
andd1791 <andd1791@gmail.com>
2024-05-05 16:51:22 +0700

    commit code init lan dau

---
+++ src/test/java/com/daoducan/tutorialcrud/TutorialCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daoducan.tutorialcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
